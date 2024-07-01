544210651db9f352a7b5db5cb07b40dc1ed92430
hardikm <hardik7203@gmail.com>
2024-07-01 15:57:38 +0530

    Done

--- create auction.py
+++ create auction.py
@@ -12,9 +12,13 @@ driver.get("https://uat.ebkray.in/")
 
 #logout and login for maker ( create auction )
 driver.find_element(By.XPATH,"//span[normalize-space()='Login']").click()
-driver.find_element(By.ID,"username").send_keys("shreya")
+driver.find_element(By.ID,"username").send_keys("hardikmaker")
 driver.find_element(By.ID,"password").send_keys("Tender@123")
 driver.find_element(By.ID,"loginBtn").click()
+driver.find_element(By.ID,"otp").click()
+time.sleep(10)
+driver.find_element(By.ID,"loginOtpBtn").click()
+
 
 #click on property
 driver.find_element(By.XPATH,"//div[normalize-space()='Property']").click()
@@ -56,7 +60,7 @@ driver.find_element(By.NAME,"sentTo").click()
 
 dropele = driver.find_element(By.NAME,"sentTo")
 select = Select(dropele)
-select.select_by_index(1)
+select.select_by_index(3)
 driver.find_element(By.ID,"remarks").send_keys("property has been sent")
 driver.find_element(By.ID,"submitBtn").click()
 time.sleep(2)
@@ -64,18 +68,26 @@ driver.find_element(By.XPATH,"//button[normalize-space()='OK']").click()
 
 #logout and login with checker
 driver.find_element(By.XPATH,"//a[normalize-space()='Logout']").click()
-driver.find_element(By.ID,"username").send_keys("vaidehi.checker")
+driver.find_element(By.ID,"username").send_keys("hardikchecker")
 driver.find_element(By.ID,"password").send_keys("Tender@123")
 driver.find_element(By.ID,"loginBtn").click()
+driver.find_element(By.ID,"otp").click()
+time.sleep(10)
+driver.find_element(By.ID,"loginOtpBtn").click()
+driver.find_element(By.XPATH,"//a[normalize-space()='To Do']").click()
+
 
 #change tab to auction
 driver.find_element(By.ID,"auctionTab").click()
 
 time.sleep(3)
 handles = driver.window_handles
-driver.switch_to.window(handles[-1])
-driver.find_element(By.ID,"action").click()
-driver.find_element(By.XPATH,"/html/body/div[3]/div/div[1]/div/div[3]/div/div[1]/div[2]/form/div/div[5]/div/div/select/option[2]").click()
+driver.switch_to.window(handles[1])
+
+driver.find_element(By.XPATH,"//select[@id='action']").click()
+acti_sel = driver.find_element(By.XPATH,"//select[@id='action']")
+select = Select(acti_sel)
+select.select_by_index(1)
 driver.find_element(By.ID,"remarks").send_keys("auction approved")
 driver.find_element(By.ID,"confirmed").click()
 driver.find_element(By.ID,"submitBtn").click()
